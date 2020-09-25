66909b89a158f86636e44ec86a7eedaaca92144a
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2020-09-26 00:02:21 +0800

    initial commit add  * app.js  * package.json

---
+++ app.js
@@ -0,0 +1,90 @@
+// modules
+const	express 	= require('express'),
+		mongoose 	= require('mongoose'),
+		axios 		= require('axios'),
+		bodyParser 	= require('body-parser'),
+		passport 	= require('passport'),
+		methodOverride = require('method-override'),
+		localStrategy = require('passport-local'),
+		flash		= require('connect-flash'),
+		moment		= require('moment');
+
+// models
+const	User 		= require('./models/user'),
+		Campground 	= require('./models/artgallery'),
+		Comment 	= require('./models/comment')
+		seedDB		= require('./seeds');
+
+// routes
+const 	artgalleryRoutes	= require('./routes/artgallery'),
+		commentsRoutes 		= require('./routes/comments'),
+		indexRoutes			= require('./routes/index');
+
+// middlewares
+const 	middleware 	= require('./middleware');
+
+// config connect to mongodb 
+mongoose.connect('mongodb://localhost:27017/artgallery_db', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true
+});
+
+// express 
+const app = express();
+const port = 3000;
+
+// express config to view ejs
+app.set('view engine', 'ejs');
+// express - use moment
+app.locals.moment = moment;
+
+// express use the bodyparser module
+app.use(bodyParser.urlencoded({extended:true}));
+// express serve static files
+app.use(express.static(__dirname+'/public'));
+
+
+
+// passport-configuration
+app.use(require('express-session')({
+	secret: 'a picture is worth a thousand words',
+	resave: false,
+	saveUninitialized: false
+}));
+// express - use passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// express - use method-override
+app.use(methodOverride('_method'));
+
+// express - use flash
+app.use(flash());
+
+
+// clean and seed the database 
+//seedDB();
+
+// meddleware to make available of our currentUser in all routes. used in header.ejs
+app.use((req,res,next)=>{
+	res.locals.currentUser	= req.user;
+	res.locals.msg_error	= req.flash('error');
+	res.locals.msg_success 	= req.flash('success');
+	next();
+});
+
+
+ // Routes
+app.use('/',indexRoutes);
+app.use('/artgallery',artgalleryRoutes);
+app.use('/artgallery/:id/comments',commentsRoutes);
+
+
+// express listen
+app.listen(3000, ()=> {
+	console.log("Art Store Gallery Server is Listening...")
+});
