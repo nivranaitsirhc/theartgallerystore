8cccb94f07ff01e9e1f5d34774bda9f2160855be
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2020-09-26 00:29:52 +0800

    add utils/

---
+++ utils/artGalleryStatusList.js
@@ -0,0 +1,26 @@
+module.exports = [
+	{
+		name: 'None',
+		value: 0
+	},
+	{
+		name:'Commisioned',
+		value: 1
+	},
+	{
+		name:'Sold',
+		value: 2
+	},
+	{
+		name:'Reserved',
+		value: 3
+	},
+	{
+		name:'Artwork',
+		value: 4
+	},
+	{
+		name:'Unavailable',
+		value: 5
+	}
+]
---
+++ utils/validateInputUrlPath.js
@@ -0,0 +1,7 @@
+// Validate input URL
+
+let pattern = /^(https?|ftps?):\/\/+|^\/+|^\w*\/+/;
+
+document.querySelector('#image').addEventListener('onChange',{
+	
+});
