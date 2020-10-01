1a3ae7e2d31f668fe7136bc8449794041db264e3
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2020-10-01 22:40:12 +0800

    improved password reset mailer refactor index.js post request -> use async and promise

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,18 @@ const 	router		 = express.Router(),
 //middleware
 const   middleware = require('../middleware');
 
+//mail transport config
+let smtpTransport = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    type:         'OAuth2',
+    user:         process.env.NODE_MAILER_user,
+    clientSecret: process.env.NODE_MAILER_clientSecret,
+    clientId:     process.env.NODE_MAILER_clientId,
+    refreshToken: process.env.NODE_MAILER_refreshToken,
+    accessToken:  process.env.NODE_MAILER_accessToken
+  }
+});
 
 //root route
 router.get("/", (req, res)=>{
@@ -23,22 +35,21 @@ router.get("/signup", (req, res)=>{
 });
 
 //handle sign up logic
-router.post("/signup", (req, res)=>{
-  req.body.password = req.body.newUser.password
-  req.body.username = req.body.newUser.username
-  req.body.newUser.fullName  = req.body.newUser.firstName + ' ' + req.body.newUser.middleName + ' ' + req.body.newUser.lastName;
-  User.register(new User(req.body.newUser), req.body.password, (err, user)=>{
-    console.log(user)
-    if(err){
-        console.log(err);
-        return res.render("auth/signup",{page:'signup',msg_error:err.message});
-    } else {
-      passport.authenticate("local")(req,res,()=>{
-        req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.newUser.firstName);
-        res.redirect('/artgallery');
-      });
-    };
-  });
+router.post("/signup", async (req, res)=>{
+  try {
+    req.body.password = req.body.newUser.password;
+    req.body.username = req.body.newUser.username;
+    req.body.newUser.fullName  = `${req.body.newUser.firstName} ${req.body.newUser.middleName} ${req.body.newUser.lastName}`;
+    let user = await User.register(new User(req.body.newUser), req.body.password);
+    passport.authenticate("local")(req,res,()=>{
+      req.flash("success", `Successfully Signed Up! Nice to meet you ${req.body.newUser.firstName}`);
+      res.redirect('/artgallery');
+    });
+  }
+  catch(err){
+    req.flash('error',err.message);
+    res.redirect('back');
+  }
 });
 
 //show login form
@@ -54,7 +65,7 @@ router.post("/login",passport.authenticate("local",
         failureFlash: true
     }
     ), (req, res)=>{
-    req.flash('success', 'Welcome back ' + req.body.username);
+    req.flash('success', `Welcome back ${req.body.username}.`);
     res.redirect('/artgallery');
 });
 
@@ -67,78 +78,62 @@ router.get("/logout", (req, res)=>{
 
 // forgot password
 router.get("/forgot", (req,res)=>{
-res.render('./auth/forgot',{page:'auth'});
+  res.render('./auth/forgot',{page:'auth'});
 });
 
 // forgot password - send email
-router.post("/forgot",(req,res,next)=>{
-  async.waterfall([
-    function(done){
-      crypto.randomBytes(20,(err,buf)=>{
-        let token = buf.toString('hex');
-        done(err,token);
-      });
-    },
-    function(token,done){
-      User.find({email:req.body.email})
-      .then(foundUser=>{
-        user=foundUser[0]
-        if(!user){
-          req.flash('error','No account exist with that email address');
-          return res.redirect('/forgot');
-        }
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = Date.now() + 360000 //1hour
-
-        user.save(err=>{
-          done(err,token,user);
-        });
-      })
-      .catch(err=>{
-        req.flash('error', err.message);
-        res.redirect('/forgot');
-      })
-    },
-    function(token,user,done){
-      let smtpTransport = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-          user: process.env.NODE_MAILER_user,
-          pass: process.env.NODE_MAILER_pass
-        }
-      });
-      let mailOptions = {
-        to: user.email,
-        from: process.env.NODE_MAILER_user,
-        subject: 'The App Store Gallery - Password Reset',
-        text: 'Hi '+user.firstName+',\n\n'+
-              'You are receiving this because you (or someone else) have requested to reset your password at '+ req.headers.host +'\n'+
-              'Please click  on the following link, or paste this into your browser to complete the process '+
-               'https://' + req.headers.host + '/reset/' + token + '\n\n'+
-              'If you did not request this, please ignore this email..'
-      };
-      smtpTransport.sendMail(mailOptions)
-      .then(()=>{
-        req.flash('success','An e-mail has been sent to '+ user.email + '. Please follow the instructions on the email your receive.');
-        return res.redirect('/forgot');    
-      })
-      .catch((err)=>{
-        req.flash('error',err.message);
-        return res.redirect('/forgot');
-      })
-    },
-
-  ],err=>{
-    if(err) return next(err);
-    res.redirect('/forgot');
-  });
+router.post("/forgot",async (req,res)=>{
+  try {
+    let token = await crypto.randomBytes(20).toString('hex');
+    let user = await User.find({email:req.body.email});
+    user = user[0];
+    if(!user) throw {message:'No account exist with that email address'};
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 360000 * 5;//1hour
+    user.save();
+    let mailOptions = {
+      to: user.email,
+      from: process.env.NODE_MAILER_user,
+      subject: 'The Art Store Gallery - Password Reset',
+      text: `Hi ${user.firstName},\n\n`+
+            `You are receiving this email because you (or someone else) have requested to reset your password at ${req.headers.host}.\n\n`+
+            `Please click  on the following link, or paste this into your browser to complete the password-reset process.\n`+
+            `https://${req.headers.host}/reset/${token}\n\n`+
+            `The link will expire in ${user.resetPasswordExpires}\n\n`+
+            `Note: If you did not make this request, please disregard and delete this email..\n\n`+
+            `Warm Regards,\n\n\n`+
+            `The Art Store Gallery Team`
+    };
+    await smtpTransport.set('oauth2_provision_cb', (user, renew, callback)=>{
+      let accessToken = userTokens[user];
+        if(!accessToken){
+          return callback(new Error('Unknown user'));
+        }else{
+          return callback(null, accessToken);
+      }
+    });
+    await smtpTransport.sendMail(mailOptions)
+    .then(()=>{
+      req.flash('success',`An e-mail has been sent to ${user.email}. Please follow the instructions on the email you receive.`);
+      return res.redirect('/forgot');    
+    })
+  }
+  catch(err){
+    req.flash('error',err.message);
+    res.redirect('back');
+  }
 });
 
 // reset password - new password
 router.get('/reset/:token', (req,res)=>{
   User.findOne({resetPasswordToken: req.params.token,resetPasswordExpires: {$gt:Date.now()}})
   .then((user)=>{
-    res.render('./auth/reset',{page:'auth',token:req.params.token})
+    if(!user<1){
+      res.render('./auth/reset',{page:'auth',token:req.params.token})
+    } else {
+      req.flash('error', 'You accessing an expired page..')
+      res.redirect('/artgallery');
+    }
   })
   .catch((err)=>{
     console.log(err);
@@ -148,43 +143,43 @@ router.get('/reset/:token', (req,res)=>{
 });
 
 // reset password - update password
-router.post('/reset/:token', (req,res)=>{
-  User.findOne({resetPasswordToken:req.params.token,resetPasswordExpires: {$gt:Date.now()}})
-  .then((user)=>{
-    if(req.body.newpassword === req.body.confirmpassword){
-      user.setPassword(req.body.newpassword)
-      .then(()=>{
-        user.setPasswordToken = undefined;
-        user.setPasswordTokenExpires = undefined;
-        user.save()
-        .then(()=>{
-          req.logIn(user,(err)=>{
-            if(err){              
-              console.log(err)
-              req.flash('error',err.message);
-              return res.redirect('back');
-            };
-            req.flash('success','You have successfully change your password..')
-            res.redirect('/artgallery');
-          });
-        });
-      })
-      .catch((err)=>{
-        console.log(err)
-        req.flash('error',err.message);
-        return res.redirect('back');
+router.post('/reset/:token', async (req,res)=>{
+  try {
+    let user = await User.findOne({resetPasswordToken:req.params.token,resetPasswordExpires: {$gt:Date.now()}});
+    if(req.body.newpassword !== req.body.confirmpassword) throw {message:'Password does not match.'};
+    user.setPassword(req.body.newpassword);
+    user.setPasswordToken = undefined;
+    user.setPasswordTokenExpires = undefined;
+    await user.save();
+    req.logIn(user,()=>{
+      req.flash('success','You have successfully change your password..')
+      let mailOptions = {
+        to: user.email,
+        from: process.env.NODE_MAILER_user,
+        subject: 'The Art Store Gallery - Password Reset',
+        text: `Hi ${user.firstName},\n\n`+
+              'Congratulations! You have sucessfully reset your password\n\n'+
+              'Warm Regards,\n\n'+
+              'The Art Store Gallery Team'
+      };
+      smtpTransport.set('oauth2_provision_cb', (user, renew, callback)=>{
+        let accessToken = userTokens[user];
+        if(!accessToken){
+          return callback(new Error('Unknown user'));
+        }else{
+          return callback(null, accessToken);
+        }
       });
-    }else{
-      req.flash('error','Password do not match..')
-      return res.redirect('back');
-    }
-  })
-  .catch((err)=>{
-    console.log(err)
+      smtpTransport.sendMail(mailOptions)
+      .then(()=>{
+        res.redirect('/artgallery');
+      });
+    });
+  }
+  catch(err){
     req.flash('error',err.message);
     res.redirect('back');
-  })
+  }
 });
-
 
 module.exports = router;
