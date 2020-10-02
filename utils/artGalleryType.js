7e0f06ed2164c64fb43500bf1aba4d9bc5579665
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2020-10-02 19:35:57 +0800

    add utils/artGalleryType.js note* forgot to include in previous commit, this file is required

---
+++ utils/artGalleryType.js
@@ -0,0 +1,19 @@
+//Art Type
+module.exports = [
+	{
+		index: 0,
+		name: 'None'
+	},
+	{
+		index: 1,
+		name:'Commisioned'
+	},
+	{
+		index: 2,
+		name:'Artwork'
+	},
+	{	
+		index: 3,
+		name:'Others'
+	}
+]
